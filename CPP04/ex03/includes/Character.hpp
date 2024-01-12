5edc6665d802712035e4f379246e75880949e3d1
Galo joel Macias zambrano <gmacias-@car8s1.42barcelona.com>
2024-01-12 15:23:42 +0100

    virtuals OK

--- CPP04/ex03/includes/Character.hpp
+++ CPP04/ex03/includes/Character.hpp
@@ -2,21 +2,7 @@
 # define CHARACTER_HPP_
 
 # include "AMateria.hpp"
-
-# include <string>
-
-class AMateria;
-
-class ICharacter
-{
-	public:
-		virtual ~ICharacter() {};
-
-		virtual const std::string&	getName() const = 0;
-		virtual void				equip(AMateria* m) = 0;
-		virtual void				unequip(int idx) = 0;
-		virtual void				use(int idx, ICharacter& target) = 0;
-};
+# include "ICharacter.hpp"
 
 class Character : public ICharacter
 {
--- CPP04/ex03/includes/MateriaSource.hpp
+++ CPP04/ex03/includes/MateriaSource.hpp
@@ -4,15 +4,7 @@
 # include <string>
 
 # include "AMateria.hpp"
-
-class IMateriaSource
-{
-	public:
-		virtual ~IMateriaSource() {};
-
-		virtual void		learnMateria(AMateria*) = 0;
-		virtual AMateria*	createMateria(const std::string& type) = 0;
-};
+# include "IMateriaSource.hpp"
 
 class MateriaSource : public IMateriaSource
 {
