61a22ece1170f0eee0bd94ef908d4fce750178c3
gmacias <ilsmaciaszambranogalo@gmail.com>
2024-01-30 23:36:30 +0100

    CPP07

---
+++ CPP07/ex00/includes/whatever.hpp
@@ -0,0 +1,29 @@
+
+#ifndef WHATEVER_HPP_
+# define WHATEVER_HPP_
+
+template<typename T>
+void swap(T& a, T& b)
+{
+	T	tmp;
+
+	tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template<typename T>
+const T& max(const T& x, const T& y)
+{
+	if (x > y) return (x);
+	return (y);
+}
+
+template<typename T>
+const T& min(const T& x, const T& y)
+{
+	if (x < y) return (x);
+	return (y);
+}
+
+#endif
---
+++ CPP07/ex02/src/main.cpp
@@ -0,0 +1,34 @@
+
+#include "iter.hpp"
+
+#include <iostream>
+
+template<typename T>
+void debug(const T& var)
+{
+	std::cout << var << std::endl;
+}
+
+template<typename T>
+void increment(const T& var)
+{
+	++(const_cast<T&>(var));
+}
+
+int main()
+{
+	int arrInt[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	std::string arrString[4] = {"abc", "dfg", "hij", "klm"};
+
+	std::cout << "INT ARRAY:\n" << std::endl;
+	::iter(arrInt, 10, &debug);
+
+	std::cout << "\nINT ARRAY (after increment):\n" << std::endl;
+	::iter(arrInt, 10, &increment);
+	::iter(arrInt, 10, &debug);
+
+	std::cout << "\nSTRING ARRAY:\n" << std::endl;
+	::iter(arrString, 4, &debug);
+
+	return (0);
+}
