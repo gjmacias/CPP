0f3437e98e50c13b8943132c200c79a5362dc0e2
Galo joel Macias zambrano <gmacias-@car8s3.42barcelona.com>
2024-02-09 09:44:48 +0100

    Better main's CPP05

--- CPP05/ex02/src/main.cpp
+++ CPP05/ex02/src/main.cpp
@@ -7,79 +7,8 @@
 
 #include <iostream>
 
-int main() {
-	std::cout << "\n========== BUREAUCRAT ==========\n" << std::endl;
-
-	Bureaucrat *bob = new Bureaucrat("Bob", 2);
-	try {
-		std::cout << "1: " << *bob << std::endl;
-		bob->increment();
-		std::cout << "2: " << *bob << std::endl;
-		bob->increment();
-		std::cout << "3: " << *bob << std::endl;
-		bob->increment();
-		std::cout << "4: " << *bob << std::endl;
-	} catch (std::exception& e) {
-		std::cout << e.what() << std::endl;
-	}
-	delete bob;
-
-	std::cout << std::endl;
-
-	Bureaucrat *jim = new Bureaucrat("Jim", 10);
-	try {
-		std::cout << "1: " << *jim << std::endl;
-		jim->increment();
-		std::cout << "2: " << *jim << std::endl;
-		jim->increment();
-		std::cout << "3: " << *jim << std::endl;
-		jim->increment();
-		std::cout << "4: " << *jim << std::endl;
-	} catch (std::exception& e) {
-		std::cout << e.what() << std::endl;
-	}
-	delete jim;
-
-	std::cout << std::endl;
-
-	Bureaucrat *luffy = new Bureaucrat("Luffy", 145);
-	try {
-		std::cout << "1: " << *luffy << std::endl;
-		luffy->decrement();
-		std::cout << "2: " << *luffy << std::endl;
-		luffy->decrement();
-		std::cout << "3: " << *luffy << std::endl;
-		luffy->decrement();
-		std::cout << "4: " << *luffy << std::endl;
-		luffy->decrement();
-		std::cout << "5: " << *luffy << std::endl;
-		luffy->decrement();
-		std::cout << "6: " << *luffy << std::endl;
-		luffy->decrement();
-		std::cout << "7: " << *luffy << std::endl;
-	} catch (std::exception& e) {
-		std::cout << e.what() << std::endl;
-	}
-	delete luffy;
-
-	std::cout << std::endl;
-
-	try {
-		Bureaucrat yuno("Yuno", 151);
-		std::cout << "HELLO WORLD!!!" << std::endl;
-	} catch (std::exception& e) {
-		std::cout << e.what() << std::endl;
-	}
-
-	std::cout << std::endl;
-
-	try {
-		Bureaucrat yuno("Yuno", -1);
-		std::cout << "HELLO WORLD!!!" << std::endl;
-	} catch (std::exception& e) {
-		std::cout << e.what() << std::endl;
-	}
-
+int main()
+{
 	std::cout << "\n========== FORM ==========\n" << std::endl;
 
 	Bureaucrat* b150 = new Bureaucrat("b150", 150);
@@ -139,18 +68,15 @@ int main() {
 	std::cout << std::endl;
 	std::cout << "--------- RobotomyRequestForm ---------" << std::endl;
 
-	for (int i = 0; i < 6; i++)
-	{
-		try { // sign
-			b1->signForm(*neighbor);
-		} catch (std::exception& e) {
-			std::cout << e.what() << std::endl;
-		}
-		try { // execute
-			b1->executeForm(*neighbor);
-		} catch (std::exception& e) {
-			std::cout << e.what() << std::endl;
-		}
+	try { // sign
+		b1->signForm(*neighbor);
+	} catch (std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+	try { // execute
+		b1->executeForm(*neighbor);
+	} catch (std::exception& e) {
+		std::cout << e.what() << std::endl;
 	}
 
 	std::cout << std::endl;
@@ -181,5 +107,5 @@ int main() {
 	delete b100;
 	delete b1;
 
-	return 0;
+	return (0);
 }
--- CPP05/ex03/src/main.cpp
+++ CPP05/ex03/src/main.cpp
@@ -8,79 +8,8 @@
 
 #include <iostream>
 
-int main() {
-	std::cout << "\n========== BUREAUCRAT ==========\n" << std::endl;
-
-	Bureaucrat *bob = new Bureaucrat("Bob", 2);
-	try {
-		std::cout << "1: " << *bob << std::endl;
-		bob->increment();
-		std::cout << "2: " << *bob << std::endl;
-		bob->increment();
-		std::cout << "3: " << *bob << std::endl;
-		bob->increment();
-		std::cout << "4: " << *bob << std::endl;
-	} catch (std::exception& e) {
-		std::cout << e.what() << std::endl;
-	}
-	delete bob;
-
-	std::cout << std::endl;
-
-	Bureaucrat *jim = new Bureaucrat("Jim", 10);
-	try {
-		std::cout << "1: " << *jim << std::endl;
-		jim->increment();
-		std::cout << "2: " << *jim << std::endl;
-		jim->increment();
-		std::cout << "3: " << *jim << std::endl;
-		jim->increment();
-		std::cout << "4: " << *jim << std::endl;
-	} catch (std::exception& e) {
-		std::cout << e.what() << std::endl;
-	}
-	delete jim;
-
-	std::cout << std::endl;
-
-	Bureaucrat *luffy = new Bureaucrat("Luffy", 145);
-	try {
-		std::cout << "1: " << *luffy << std::endl;
-		luffy->decrement();
-		std::cout << "2: " << *luffy << std::endl;
-		luffy->decrement();
-		std::cout << "3: " << *luffy << std::endl;
-		luffy->decrement();
-		std::cout << "4: " << *luffy << std::endl;
-		luffy->decrement();
-		std::cout << "5: " << *luffy << std::endl;
-		luffy->decrement();
-		std::cout << "6: " << *luffy << std::endl;
-		luffy->decrement();
-		std::cout << "7: " << *luffy << std::endl;
-	} catch (std::exception& e) {
-		std::cout << e.what() << std::endl;
-	}
-	delete luffy;
-
-	std::cout << std::endl;
-
-	try {
-		Bureaucrat yuno("Yuno", 151);
-		std::cout << "HELLO WORLD!!!" << std::endl;
-	} catch (std::exception& e) {
-		std::cout << e.what() << std::endl;
-	}
-
-	std::cout << std::endl;
-
-	try {
-		Bureaucrat yuno("Yuno", -1);
-		std::cout << "HELLO WORLD!!!" << std::endl;
-	} catch (std::exception& e) {
-		std::cout << e.what() << std::endl;
-	}
-
+int main()
+{	
 	std::cout << "\n========== FORM ==========\n" << std::endl;
 
 	std::cout << "CREATE BUREAUCRATS:" << std::endl;
